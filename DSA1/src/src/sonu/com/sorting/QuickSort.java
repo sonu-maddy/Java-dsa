f9cb738823ed86529a18e233f79252c5c30165cf
Sonu Maddheshiya <96307808+sonu-maddy@users.noreply.github.com>
2025-07-17 22:41:46 +0530

    space added

--- DSA1/src/src/sonu/com/sorting/QuickSort.java
+++ DSA1/src/src/sonu/com/sorting/QuickSort.java
@@ -42,7 +42,7 @@ public class QuickSort {
 
     private static void swap(int[] arr, int i, int j) {
         int temp = arr[i];
-        arr[i] = arr[j];
+        arr[i] = arr[j]; 
         arr[j] = temp;
     }
 }
